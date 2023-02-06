5748c8885a61cad1e578d6e397622499ebc6b6ab
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2023-02-06 01:20:12 -0800

    No Duplicasy

--- src/bridgelabz/services/AddressBookMain.java
+++ src/bridgelabz/services/AddressBookMain.java
@@ -1,18 +1,17 @@
-/**
- * Purpose: The purpose of this class to create Address Book System and add
- * multiple persons details with their state or city reference.
- * @author Rahul
- */
 package bridgelabz.services;
+
 import bridgelabz.model.Person;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+
 public class AddressBookMain {
     private static Scanner scanner = new Scanner(System.in);
-    private static Map<String ,Person> personMap = new HashMap();
+    private static Map<String, Person> personMap = new HashMap();
     private static Map<String, Map<String, Person>> addressBookMap = new HashMap();
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         boolean isExit = false;
 
         do {
@@ -31,13 +30,23 @@ public class AddressBookMain {
                     break;
                 case 'E':
                     //edit
+                    System.out.print("\nEnter the first name of the person to edit : ");
+                    String firstName = scanner.nextLine();
+                    System.out.print("\nEnter the city name of the person to edit : ");
+                    String cityName = scanner.nextLine();
+                    editContact(firstName,cityName);
                     break;
                 case 'D':
                     //delete
+                    System.out.print("\nEnter the first name of the person to edit : ");
+                    String personName = scanner.nextLine();
+                    System.out.print("\nEnter the city name of the person to edit : ");
+                    String city = scanner.nextLine();
+                    deletePerson(personName,city);
                     break;
                 case 'S':
                     //Show
-                    System.out.println("\n\t\t" + personMap.get(personMap.size() - 1).toString());
+                    System.out.println("\n\t\t" + addressBookMap.toString());
                     break;
                 case 'Q':
                     //quit
@@ -48,15 +57,9 @@ public class AddressBookMain {
             }
         } while (!isExit);
 
-
     }
-    private static void editContact() {
-        System.out.print("\nEnter the first name of the person to edit : ");
-        String firstName = scanner.nextLine();
-        System.out.print("\nEnter the city name of the person to edit : ");
-        String cityName = scanner.nextLine();
+    private static void editContact(String firstName, String cityName) {
         personMap = addressBookMap.get(cityName);
-        ;
         System.out.println(personMap.toString());
         if (addressBookMap.get(cityName).get(firstName) != null) {
             Person editedPerson = contactFields();
@@ -67,29 +70,36 @@ public class AddressBookMain {
         }
         System.out.println("\n\t\t" + addressBookMap.toString());
     }
+
     private static Person contactFields() {
         Person person = new Person();
         System.out.print("Enter First Name : ");
         person.setFirstName(scanner.nextLine());
+
         System.out.print("Enter Last Name : ");
         person.setLastName(scanner.nextLine());
+
         System.out.print("Enter Address : ");
         person.setAddress(scanner.nextLine());
+
+        System.out.print("Enter State : ");
+        person.setState(scanner.nextLine());
+
         System.out.print("Enter City : ");
         person.setCity(scanner.nextLine());
+
         System.out.print("Enter ZipCode : ");
         person.setZip(scanner.nextLine());
+
         System.out.print("Enter Phone Number : ");
         person.setPhone(scanner.nextLine());
         return person;
     }
-    private static void deletePerson() {
-        System.out.print("\nEnter the first name of the person to delete : ");
-        String firstName = scanner.nextLine();
-        Person newPerson = personMap.get(firstName);
+    private static void deletePerson(String firstName, String cityName) {
+        Person newPerson = addressBookMap.get(cityName).get(firstName);
         System.out.println(newPerson.toString());
-        if (personMap.get(firstName) != null) {
-            personMap.remove(firstName);
+        if (addressBookMap.get(cityName).get(firstName) != null) {
+            addressBookMap.get(cityName).remove(firstName);
             System.out.println("Deleted Successfully");
         } else {
             System.out.println("Record not exist");
